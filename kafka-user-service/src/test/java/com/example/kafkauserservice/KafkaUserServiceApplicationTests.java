d6c9d21b4011b5d072c86c37519e537b0665ef7b
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-12-17 14:32:39 +0900

    init: 프로젝트 생성

---
+++ kafka-user-service/src/test/java/com/example/kafkauserservice/KafkaUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkauserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
